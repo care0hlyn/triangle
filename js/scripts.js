ffe1816fdecea9642675b3d779ac7ea37fe6e86c
Carolyn Huynh <ckhuynh20@gmail.com>
2014-07-16 23:13:11 -0700

    Second commit, all specs passed

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,15 @@
-var triangle = function(side1, side2, side3) {
+var triangle = function(side1,side2,side3) {
 
-// var equilateral;
-// var isosceles;
-// var scalene;
-// var result;
-
-
-var side1 = side1;
-var side2 = side2;
-var side3 = side3;
-
-if ((side1 && side2 && side3) === (side1 && side2 && side3)) {
+if ((((side1 + side2) < side3) || ((side2 + side3) < side1) || ((side3 + side2) < side1))) {
+	return "Not a triangle";
+} else if (side1 === side2 && side1 === side3) {
 	return "equilateral";
+} else if (side1 === side3 || side2 === side1 || side3 === side2) {
+	return "isoceles"; 
+} else if ((side1 !== side2 && side1 !== side3) && (side2 !== side3)) {
+	return "scalene";
+} else {
+	return false;
 }
-
-
 };
+
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,14 @@
-describe('triangle', function() {
-  it("Is equilateral if all sides of a triangle are equal", function() {
-    triangle("5, 5, 5").should.eql("equilateral");
+describe("triangle", function() {
+  it("All sides of a triangle are equal", function() {
+    triangle(5, 5, 5).should.equal("equilateral");
+  });
+  it("If two sides of a triangle are equal", function() {
+  	triangle(5, 7, 5).should.equal("isoceles");
+  });
+  it("If no sides are equal", function() {
+  	triangle(6, 5, 9).should.equal("scalene");
+  });
+  it("If one side is at least as long as the other two sides combined", function() {
+  	triangle(2, 2, 8).should.equal("Not a triangle");
   });
 });
