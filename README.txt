d1a69698fdd4eb61561ce06bec96d8f6acb39b4a
Carolyn Huynh <ckhuynh20@gmail.com>
2014-07-17 22:48:37 -0700

    Final commit, specs pass, jQuery works

---
+++ README.txt
@@ -0,0 +1,2 @@
+This is a web application that allows the user to input all three sides of a triangle to determine what type of triangle it is: equilateral, isoceles, scalene or "NOT A TRIANGLE".
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
 var triangle = function(side1,side2,side3) {
 
 if ((((side1 + side2) < side3) || ((side2 + side3) < side1) || ((side3 + side2) < side1))) {
-	return "Not a triangle";
+	return "NOT A TRIANGLE";
 } else if (side1 === side2 && side1 === side3) {
-	return "equilateral";
+	return "EQUILATERAL";
 } else if (side1 === side3 || side2 === side1 || side3 === side2) {
-	return "isoceles"; 
+	return "ISOCELES"; 
 } else if ((side1 !== side2 && side1 !== side3) && (side2 !== side3)) {
-	return "scalene";
+	return "SCALENE";
 } else {
 	return false;
 }
 };
 
+$(document).ready(function(){
+	$('form#triangle').submit(function(event){
+		event.preventDefault();
+
+	var side1 = parseInt($('input#side1').val());
+	var side2 = parseInt($('input#side2').val());
+	var side3 = parseInt($('input#side3').val());
+
+	var result = triangle(side1, side2, side3);
+
+
+	$(".type").text(result);
+    $("#result").show();
+
+	});
+});
